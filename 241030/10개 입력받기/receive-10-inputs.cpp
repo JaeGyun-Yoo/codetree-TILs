7d6c48ce9361a995e248ebd207b6737c88852dca
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-10-30 15:45:45 +0900

    Title: 10개 입력받기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241030/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    int sum = 0;
+    int count = 0;
+    for(int i=0; i<10; i++)
+    {
+        cin >> arr[i];
+        if(arr[i] == 0) {count = i; break;}
+    }
+    for(int i=0; i<count; i++)
+    {
+        sum += arr[i];
+    }
+    float avg = (float)sum / count;
+    cout << fixed;
+    cout.precision(1);
+    cout << sum << ' ' << avg;
+    
+    return 0;
+}
