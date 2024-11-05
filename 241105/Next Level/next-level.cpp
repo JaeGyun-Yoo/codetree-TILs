f3c0e270590b31c58e972f0e32bd892c0e18698c
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-11-05 15:24:46 +0900

    Title: Next Level, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241105/Next Level/next-level.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+struct obj
+{
+    string id = "codetree";
+    int level = 10;
+};
+
+int main()
+{
+    obj a,b;
+    cin >> b.id;
+    cin >> b.level;
+    cout << "user " << a.id << " lv " << a.level << endl;
+    cout << "user " << b.id << " lv " << b.level << endl;
+}
