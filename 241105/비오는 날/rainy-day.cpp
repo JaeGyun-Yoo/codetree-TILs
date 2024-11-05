cabca213bfc8a5c44de3bf356c3f560216847843
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-11-05 15:57:38 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/비오는 날/rainy-day.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+struct weather
+{
+    int year;
+    int month;
+    int day;
+    string week;
+    string what;
+};
+
+bool compare(weather* cur, weather* nxt)
+{
+    if(cur->year != nxt->year) {
+        return cur->year < nxt->year;
+    }
+    if(cur->month != nxt->month) {
+        return cur->month < nxt->month;
+    }
+    return cur->day < nxt->day;
+}
+int main()
+{
+    int n; cin >> n;
+    weather* arr[100];
+    for(int i=0; i<n; i++)
+    {
+        string a,b,c; cin >> a >> b >> c;
+        arr[i] = new weather;
+        arr[i]->year = stoi(a.substr(0,4));
+        arr[i]->month = stoi(a.substr(5,2));
+        arr[i]->day = stoi(a.substr(8,2));
+        arr[i]->week = b;
+        arr[i]->what = c;
+    }
+    sort(arr, arr+n, compare);
+    for(int i=0; i<n; i++)
+    {
+        if(arr[i]->what == "Rain")
+        {
+            cout<<arr[i]->year<<"-"<<arr[i]->month<<"-"<<arr[i]->day<<" "<<arr[i]->week<<" "<<arr[i]->what;
+            break;
+        }
+    }
+}
