8e3dc3713f205c3527650cdcb5ac0dff4af57c48
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-11-05 15:13:19 +0900

    Title: 2개씩 그룹짓기, Time: 6ms, Memory: 0MB, Status: Memory Limit Exceed - CodeTree

---
+++ 241105/2개씩 그룹짓기/group-of-pairs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int arr[2000];
+int main()
+{
+    int n; cin >> n;
+    int* sum = new int[n];
+    n *= 2;
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    sort(arr,arr+n);
+    for(int i=0; i<n; i++)
+    {
+        sum[i] = arr[i] + arr[n-i-1];
+    }
+    sort(sum,sum+(n/2));
+    cout << sum[n/2 - 1];
+}
