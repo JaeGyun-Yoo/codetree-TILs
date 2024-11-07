48256232800ddbebecc50d010581ddc5a763aa6b
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-11-07 14:00:09 +0900

    Title: Date to Date, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241107/Date to Date/date-to-date.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int m1,d1,m2,d2; cin >> m1 >> d1 >> m2 >> d2;
+    int month = m1; int day = d1;
+    int answer = 0;
+    while(1)
+    {
+        if(month == m2 && day == d2) break;
+        if(month == 2)
+        {
+            if(day == 29)
+            {
+                month++;
+                day=1;
+            }
+        }
+        else if(month % 2 == 0)
+        {
+            if(day == 31)
+            {
+                month++;
+                day=1;
+            }
+        }
+        else if(month % 2 == 1)
+        {
+            if(day == 32)
+            {
+                month++;
+                day=1;
+            }
+        }
+        answer++;
+        day++;
+        if(month == m2 && day == d2) break;
+    }
+    cout << answer;
+}
