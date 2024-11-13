7777bfb630eed97bcf30128c28d5d423cd1f4c1d
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-11-13 18:31:33 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 18ms, Memory: 16MB, Status: Passed - CodeTree

--- 241113/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ 241113/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -9,6 +9,7 @@ int main()
     for(int i=0; i<2; i++)
     {
         int a,b,c,d; cin >> a >> b >> c >> d;
+        a += 1000; b += 1000; c += 1000; d += 1000;
         for(int j=a; j<c; j++)
         {
             for(int k=b; k<d; k++)
@@ -18,6 +19,8 @@ int main()
         }
     }
     int a,b,c,d; cin >> a >> b >> c >> d;
+            a += 1000; b += 1000; c += 1000; d += 1000;
+
     for(int i=a; i<c; i++)
     {
         for(int j=b; j<d; j++)
