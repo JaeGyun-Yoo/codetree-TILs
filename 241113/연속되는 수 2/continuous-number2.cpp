c08a33d9875a49c43e643918a9cccaa7a1ccc99c
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-11-13 19:07:31 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241113/연속되는 수 2/continuous-number2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int arr[1000];
+struct target
+{
+    int idx;
+    int value;
+};
+
+int main()
+{
+    int n; cin >> n;
+    for(int i=0; i<n; i++)
+    {
+        cin >> arr[i];
+    }
+    target a;
+    a.value = arr[0];
+    a.idx = 0;
+    int max_length = 0;
+    for(int i=0; i<n; i++)
+    {
+        if(a.value != arr[i])
+        {
+            if(max_length < i - a.idx)
+                max_length = i - a.idx;
+            a.value = arr[i];
+            a.idx = i;
+        }
+    }
+    cout << max_length;
+}
