d708936a4e380d3c88fc05a218db0c8f718f079a
YOUJAEGYUN <148213388+YOUJAEGYUN@users.noreply.github.com>
2024-11-13 17:12:27 +0900

    Title: 최대로 겹치는 구간, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241113/최대로 겹치는 구간/maximum-overlapped-segments.cpp
+++ 241113/최대로 겹치는 구간/maximum-overlapped-segments.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+int arr[201];
 
 bool compare(int& a, int& b)
 {
-    return a >= b;
+    return a > b;
 }
+
 int main()
 {
     int n; cin >> n;
-    int arr[200] = {};
     for(int i=0; i<n; i++)
     {
         int start, end; cin >> start >> end;
+        start += 100; end += 100;
         for(start; start<end; start++)
         {
             arr[start]++;
         }
     }
-    sort(arr,arr+200, compare);
+    sort(arr,arr+201, compare);
     cout << arr[0];
 }
